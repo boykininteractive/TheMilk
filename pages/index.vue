792c71e8381646a8324260eb362324609f82c7e8
TJ Boykin <tjb@boykinllc.com>
2020-11-10 12:09:38 -0500

    remove extra styles / code

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="container">
+  <div class="flex justify-center align-center text-center mx-auto py-12">
     <div>
       <LazyLogo />
-      <h1 class="title">boykin.site</h1>
-      <h2 class="subtitle">My stylish Nuxt.js project</h2>
+      <div class="text-6xl text-secondary-400 font-bold">boykin.site</div>
       <LazyPalette />
     </div>
   </div>
@@ -14,39 +13,4 @@ export default {};
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
