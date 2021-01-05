97f964067785cd7c60974960757c4e0c0070cac9
TJ <tjb@boykinllc.com>
2021-01-04 19:30:45 -0500

    clean up typo

--- frontend/components/Subscribe.vue
+++ frontend/components/Subscribe.vue
@@ -3,9 +3,6 @@
     <!--- SECTION TITLE --->
     <LazySectionTitle :content="content.subscribe_title" />
 
-      {{ content.audience_subtitle }}
-    </div>
-
     <!--- CONTENT AREA --->
     <div
       class="max-w-screen-md mx-auto -mt-6 text-xl text-center text-gray-600 lg:text-2xl"
@@ -13,15 +10,31 @@
     ></div>
 
     <div class="box-border text-base font-normal leading-6 text-left">
-      <form class="box-border flex flex-row flex-wrap items-center justify-center mt-8 text-base font-normal leading-6 text-left">
-        <div class="box-border relative flex flex-wrap items-stretch mb-4 text-base font-normal leading-6 text-left">
-          <input id="email" type="email" placeholder="Enter your email ..." aria-label="Enter your email ..." aria-describedby="subscribe" required="required" class="box-border relative flex-1 block w-64 h-12 px-6 py-0 m-0 overflow-visible overflow-x-visible overflow-y-visible text-sm font-normal leading-5 align-middle bg-white border-t border-b border-l border-r border-solid rounded-full outline-none bg-clip-padding"> 
-          <button type="submit" id="subscribe" class="box-border right-0 z-20 w-32 h-12 m-0 -ml-10 overflow-visible overflow-x-visible overflow-y-visible text-base font-normal leading-6 text-center text-white normal-case bg-scroll bg-repeat bg-auto border-t-0 border-b-0 border-l-0 border-r-0 border-white rounded-full outline-none cursor-pointer bg-primary-blue bg-clip-border bg-none" >
-              Subscribe
-            </button>
-            </div>
-            </form>
-            </div>
+      <form
+        class="box-border flex flex-row flex-wrap items-center justify-center mt-8 text-base font-normal leading-6 text-left"
+      >
+        <div
+          class="box-border relative flex flex-wrap items-stretch mb-4 text-base font-normal leading-6 text-left"
+        >
+          <input
+            id="email"
+            type="email"
+            placeholder="Enter your email ..."
+            aria-label="Enter your email ..."
+            aria-describedby="subscribe"
+            required="required"
+            class="box-border relative flex-1 block w-64 h-12 px-6 py-0 m-0 overflow-visible overflow-x-visible overflow-y-visible text-sm font-normal leading-5 align-middle bg-white border-t border-b border-l border-r border-solid rounded-full outline-none bg-clip-padding"
+          />
+          <button
+            type="submit"
+            id="subscribe"
+            class="box-border right-0 z-20 w-32 h-12 m-0 -ml-10 overflow-visible overflow-x-visible overflow-y-visible text-base font-normal leading-6 text-center text-white normal-case bg-scroll bg-repeat bg-auto border-t-0 border-b-0 border-l-0 border-r-0 border-white rounded-full outline-none cursor-pointer bg-primary-blue bg-clip-border bg-none"
+          >
+            Subscribe
+          </button>
+        </div>
+      </form>
+    </div>
   </section>
 </template>
 
