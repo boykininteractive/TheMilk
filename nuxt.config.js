cf059812e41ab3b73a77925d427f96f160ab1180
TJ <tjb@boykinllc.com>
2021-01-13 08:29:16 -0500

    render blocking

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
-        rel: "stylesheet",
+        rel: "preload",
+        as: "style",
         media:"all",
         href: "https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
       }
