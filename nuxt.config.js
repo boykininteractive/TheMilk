61c8af8397a4c02cfde087b872f60a24a9c9a1d9
TJ <tjb@boykinllc.com>
2021-01-13 08:19:24 -0500

    address render blocking for google fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: "stylesheet",
+        media:"all",
         href: "https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
       }
     ],
