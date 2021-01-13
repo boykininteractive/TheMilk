bdd3ae7d101e777cb43f8d6e78f5351e1641c732
TJ <tjb@boykinllc.com>
2021-01-13 08:31:20 -0500

    render blocking

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,7 @@ export default {
       {
         rel: "preload",
         as: "style",
+        onload: "this.onload=null;this.rel='stylesheet'",
         media:"all",
         href: "https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
       }
